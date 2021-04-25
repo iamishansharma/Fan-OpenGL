3b389107ebe0e9c2cd0bf2615c511b04b6dd9e02
Ishan Sharma <sharmaishan747@gmail.com>
2021-04-25 05:20:50 -0700

    Fan added

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #endif
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
 #include <iostream>
+#include <math.h>
  
 /* Global variables */
 char title[] = "Assignment 2 - Ishan Sharma 2016B2A70773P";
@@ -15,132 +16,189 @@ GLfloat Cz = 3;
  
 /* Initialize OpenGL Graphics */
 void initGL() {
-   glClearColor(0.0f, 0.0f, 0.0f, 1.0f); 
-   glEnable(GL_DEPTH_TEST);   
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glFrustum(-1,1,-1,1,2,100);
-   glMatrixMode(GL_MODELVIEW);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);   
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1,1,-1,1,2,100);
+	glMatrixMode(GL_MODELVIEW);
 
-   glEnable(GL_LIGHTING);
-   glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	//glColorMaterial(GL_FRONT, GL_DIFFUSE);
+	//glEnable(GL_COLOR_MATERIAL);
 }
 
 void RenderTableLegs()
 {
-   glPushMatrix();
-      glScalef(0.05, 1, 0.05);
-      glutSolidCube(1);
-   glPopMatrix();
+	glPushMatrix();
+		glScalef(0.05, 1, 0.05);
+		glutSolidCube(1);
+	glPopMatrix();
 }
 
 void RenderTable()
 { 
-   glPushMatrix();
-      glScalef(1, 0.05, 1);
-      glutSolidCube(1);
-   glPopMatrix();
+	glPushMatrix();
+		glScalef(1, 0.05, 1);
+		glutSolidCube(1);
+	glPopMatrix();
 
-   glPushMatrix();
-      glTranslatef(-0.5, -0.525, -0.5);
-      RenderTableLegs();
-   glPopMatrix();
+	glPushMatrix();
+		glTranslatef(-0.5, -0.525, -0.5);
+		RenderTableLegs();
+	glPopMatrix();
 
-   glPushMatrix();
-      glTranslatef(0.5, -0.525, -0.5);
-      RenderTableLegs();
-   glPopMatrix();
+	glPushMatrix();
+		glTranslatef(0.5, -0.525, -0.5);
+		RenderTableLegs();
+	glPopMatrix();
 
-   glPushMatrix();
-      glTranslatef(0.5, -0.525, 0.5);
-      RenderTableLegs();
-   glPopMatrix();
+	glPushMatrix();
+		glTranslatef(0.5, -0.525, 0.5);
+		RenderTableLegs();
+	glPopMatrix();
 
-   glPushMatrix();
-      glTranslatef(-0.5, -0.525, 0.5);
-      RenderTableLegs();
-   glPopMatrix();
+	glPushMatrix();
+		glTranslatef(-0.5, -0.525, 0.5);
+		RenderTableLegs();
+	glPopMatrix();
 }
- 
+
+void RotatingBody(unsigned int uiStacks, unsigned int uiSlices, float fA, float fB, float fC)
+{	
+	// Piece 4
+	glPushMatrix();
+		glTranslatef(0.0, 0.35, 0.15);
+		glScalef(0.15, 0.1, 0.3);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(0.0,0.4,0.55);
+		glRotatef(180,0,1,0);
+		float tStep = (3.14) / (float)uiSlices;	
+		float sStep = (3.14) / (float)uiStacks;	
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);	
+		for(float t = -3.14/2; t <= (3.14/2)+.0001; t += tStep)	
+		{		
+			glBegin(GL_TRIANGLE_STRIP);		
+			for(float s = -3.14; s <= 3.14+.0001; s += sStep)		
+			{			
+				glVertex3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));			
+				glVertex3f(fA * cos(t+tStep) * cos(s), fB * cos(t+tStep) * sin(s), fC * sin(t+tStep));		
+			}		
+			glEnd();	
+		}
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		glutSolidSphere(0.05, 5, 5);
+	glPopMatrix();
+} 
+
 void RenderFan()
 {
-   
+	glTranslatef(0.0, 0.5, 0);
+
+	// Piece 1
+	glPushMatrix();
+		glScalef(0.2, 0.4, 0.1);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	// Piece 2
+	glPushMatrix();
+		glTranslatef(0.0, 0.15, 0.1);
+		glScalef(0.2, 0.1, 0.2);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	// Piece 3
+	glPushMatrix();
+		glTranslatef(0.0, 0.25, 0.15);
+		glScalef(0.1, 0.1, 0.1);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+		glRotatef(0,0,1,0);
+		RotatingBody(15, 2, 0.35, 0.35, 0.15);
+	glPopMatrix();
 }
 
 void RenderWalls()
 {
-   glPushMatrix();
-      glTranslatef(0,0,-2);
-      glScalef(25.0, 25.0, 0.05);
-      glColor3f(1.0,0.0,0.0);
-      glutSolidCube(1);
-   glPopMatrix();
+
 }
 
 void display() 
 {
-   GLfloat Pos[] = {0,1,0,1};
-   //GLfloat Col[] = {1,0,0,1};
+	GLfloat Pos[] = {0,1,0,1};
+	//GLfloat Col[] = {1,0,0,1};
 
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-   glMatrixMode(GL_MODELVIEW);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glMatrixMode(GL_MODELVIEW);
 
-   glLoadIdentity();
-   glLightfv(GL_LIGHT0, GL_POSITION, Pos);
-   //glLightfv(GL_LIGHT0, GL_DIFFUSE, Col);
+	glLoadIdentity();
+	glLightfv(GL_LIGHT0, GL_POSITION, Pos);
+	//glLightfv(GL_LIGHT0, GL_DIFFUSE, Col);
 
-   gluLookAt(Cx,Cy,Cz,0,0,0,0,1,0);
+	gluLookAt(Cx,Cy,Cz,0,0,0,0,1,0);
 
-   // glLoadIdentity();
-   //glTranslatef(0.0, 0.0, -5);
+	//glLoadIdentity();
+	glTranslatef(0.0, -1, 0.0);
 
-   RenderTable();
-   RenderWalls();
+	RenderTable();
+
+	//RenderWalls();
+	//glRotatef(90,0,1,0);
+	RenderFan();
  
-   glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
+	glutSwapBuffers();  // Swap the front and back frame buffers (double buffering)
 }
 
 void processNormalKeys(unsigned char key, int x, int y) 
 {
-   switch(key)
-   {
-      case 'x': Cx = Cx - 0.5;
-                  break;
-      case 'X': Cx = Cx + 0.5;
-                  break;
-      case 'y': Cy = Cy - 0.5;
-                  break;
-      case 'Y': Cy = Cy + 0.5;
-                  break;
-      case 'z': Cz = Cz - 0.5;
-                  break;
-      case 'Z': Cz = Cz + 0.5;
-                  break;
-      case 27:
-      case 'q':
-      case 'Q':   exit(0);
-                  break;
-   }
+	switch(key)
+	{
+		case 'x': Cx = Cx - 0.5;
+						break;
+		case 'X': Cx = Cx + 0.5;
+						break;
+		case 'y': Cy = Cy - 0.5;
+						break;
+		case 'Y': Cy = Cy + 0.5;
+						break;
+		case 'z': Cz = Cz - 0.5;
+						break;
+		case 'Z': Cz = Cz + 0.5;
+						break;
+		case 27:
+		case 'q':
+		case 'Q':   exit(0);
+						break;
+	}
 
-   glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 void reshape(GLsizei width, GLsizei height) 
 {  
-   
+	
 }
  
 /* Main function: GLUT runs as a console application starting at main() */
 int main(int argc, char** argv) {
-   glutInit(&argc, argv);            // Initialize GLUT
-   glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH); // Enable double buffered mode
-   glutInitWindowSize(600, 600);   // Set the window's initial width & height
-   glutInitWindowPosition(100, 150); // Position the window's initial top-left corner
-   glutCreateWindow(title);          // Create window with the given title
-   //glutReshapeFunc(reshape);
-   initGL();                       // Our own OpenGL initialization
-   glutDisplayFunc(display);       // Register callback handler for window re-paint event
-   glutKeyboardFunc(processNormalKeys);
-   glutMainLoop();                 // Enter the infinite event-processing loop
-   return 0;
+	glutInit(&argc, argv);            // Initialize GLUT
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Enable double buffered mode
+	glutInitWindowSize(600, 600);   // Set the window's initial width & height
+	glutInitWindowPosition(100, 150); // Position the window's initial top-left corner
+	glutCreateWindow(title);          // Create window with the given title
+	//glutReshapeFunc(reshape);
+	initGL();                       // Our own OpenGL initialization
+	glutDisplayFunc(display);       // Register callback handler for window re-paint event
+	glutKeyboardFunc(processNormalKeys);
+	glutMainLoop();                 // Enter the infinite event-processing loop
+	return 0;
 }
